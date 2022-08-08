cca5e55d34597c276b3c8386cefbf5c74b8b1862
Maxtanie Petit Dol <108846141+maxtanielab@users.noreply.github.com>
2022-08-08 06:59:07 +0200

    Changed assets folders files

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,151 +1,110 @@
-
-import pokemonRandomCard from "./pokemonRandomCard.js";
 import pokemonCards from "./pokemonCards.js";
 
 $(async function () {
-  let res = await getPokemons("https://pokeapi-enoki.netlify.app/pokeapi.json");
-  let pokemons = res.pokemons;
+	let res = await getPokemons("https://pokeapi-enoki.netlify.app/pokeapi.json");
+	let pokemons = res.pokemons;
 
-  /* Get the main pokemon card */
-  let cards = $("#card");
-  let mainImage = $("#main-image");
-  let mainName = $("#name");
-  let mainLevel = $("#level");
-  let mainAbilities = $("#abilities");
+	let cards = $("#pokemon-random-cards");
+	let abilitiesContentOutput = "";
+	let cardActive = false;
+	let currentImageIndex = getRandomNumber(pokemons.length);
 
-  let counterSelectCard = 0;
-  // let counterStartClick = 0;
-  let counterStopClick = 0;
-  let cardActive = false;
+	//Initial random card
+	pokemonCards(pokemons[currentImageIndex], abilitiesContentOutput, cards);
 
-  let currentImageIndex = getRandomNumber(pokemons.length);
-  let abilitiesContentOutput = "";
+	let spanTimer = $("#span-timer");
+	let stopBtn = $("#stop-btn");
+	$(stopBtn).prop("disabled", true);
+	let startBtn = $("#start-btn");
 
-  pokemonRandomCard(
-    pokemons[currentImageIndex],
-    abilitiesContentOutput,
-    mainAbilities,
-    cards,
-    mainImage,
-    mainName,
-    mainLevel
-  );
+	let usedImageIndexes = [];
+	let lastFilledImageNumber = 0;
+	let intervalHandle = null;
+	let timeoutHandle = null;
 
-  let spanTimer = $("#span-timer");
+	//Start btn onclick
+	$(startBtn).on("click", function () {
+		let counter = 15;
+		currentImageIndex = getRandomNumber(pokemons.length);
 
-  let stopBtn = $("#stop-btn");
-  $(stopBtn).prop("disabled", true);
+		//pokemonRandomCard
+		pokemonCards(pokemons[currentImageIndex], abilitiesContentOutput, cards);
 
-  let startBtn = $("#start-btn");
+		$(stopBtn).prop("disabled", false);
 
-  let usedImageIndexes = [];
-  let lastFilledImageNumber = 0;
-  let intervalHandle = null;
-  let timeoutHandle = null;
+		clearInterval(intervalHandle);
+		clearTimeout(timeoutHandle);
 
-  $(startBtn).on("click", function () {
-    let counter = 15;
-    currentImageIndex = getRandomNumber(pokemons.length);
+		intervalHandle = setInterval(function () {
+			currentImageIndex = getRandomNumber(pokemons.length);
 
-    // $(cards).removeClass("hide-card");
+			pokemonCards(pokemons[currentImageIndex], abilitiesContentOutput, cards);
 
-    //pokemonRandomCard
-    pokemonRandomCard(
-      pokemons[currentImageIndex],
-      abilitiesContentOutput,
-      mainAbilities,
-      cards,
-      mainImage,
-      mainName,
-      mainLevel
-    );
+			$(spanTimer).text(`${counter--} (s)`);
+		}, 1000);
 
-    $(stopBtn).prop("disabled", false);
+		timeoutHandle = setTimeout(function () {
+			handleImageChange();
+			console.log(pokemons[currentImageIndex]);
+		}, counter * 1000);
+	});
 
-    clearInterval(intervalHandle);
-    clearTimeout(timeoutHandle);
+	//Stop btn onclick
+	$(stopBtn).on("click", function () {
+		handleImageChange();
 
-    intervalHandle = setInterval(function () {
-      currentImageIndex = getRandomNumber(pokemons.length);
+		console.log(pokemons[currentImageIndex]);
 
-      pokemonRandomCard(
-        pokemons[currentImageIndex],
-        abilitiesContentOutput,
-        mainAbilities,
-        cards,
-        mainImage,
-        mainName,
-        mainLevel
-      );
+		$(cards).on("click", function () {
+			cardActive = true;
+			cardActive ? $(this).addClass("active") : console.log(cardActive);
+			console.log(usedImageIndexes.length);
+			let selectData = $(
+				`.empty-cards-content[data-id="${usedImageIndexes.length}"] .cards`
+			);
 
-      $(spanTimer).text(`${counter--} (s)`);
-    }, 1000);
+			pokemonCards(
+				pokemons[currentImageIndex],
+				abilitiesContentOutput,
+				selectData
+			);
+		});
+	});
 
-    timeoutHandle = setTimeout(function () {
-      handleImageChange();
-      console.log(pokemons[currentImageIndex]);
-    }, counter * 1000);
-  });
+	//Handle image change
+	function handleImageChange() {
+		$(stopBtn).prop("disabled", true);
+		$(spanTimer).text("");
 
-  $(stopBtn).on("click", function () {
-    handleImageChange();
-    counterSelectCard++;
+		clearInterval(intervalHandle);
+		clearTimeout(timeoutHandle);
 
-    console.log(pokemons[currentImageIndex]);
+		if (usedImageIndexes.includes(currentImageIndex)) {
+			alert("image est déjà dans la sélection !");
+		} else {
+			usedImageIndexes.push(currentImageIndex);
+			console.log("USED", usedImageIndexes);
+			$("#image" + ++lastFilledImageNumber).attr(
+				"src",
+				pokemons[currentImageIndex].image
+			);
+		}
 
-    $(cards).on("click", function () {
-      cardActive = true;
-      cardActive ? $(this).addClass("active") : console.log(cardActive);
-      console.log(usedImageIndexes.length);
-      let selectData = $(
-        `.empty-cards-content[data-id="${usedImageIndexes.length}"] .cards`
-      );
+		if (lastFilledImageNumber === 6) {
+			$(startBtn).text("C'est fini !");
+			$(startBtn).prop("disabled", true);
+		}
+	}
 
-      pokemonCards(
-        pokemons[currentImageIndex],
-        abilitiesContentOutput,
-        selectData
-      );
+	//Fetch and return pokemon API
+	async function getPokemons(url) {
+		let body = await fetch(url);
+		let pokemons = await body.json();
+		return pokemons;
+	}
 
-      // $(".empty-cards .cards").map((el, index) => {
-      //   return console.log($(".empty-cards .cards").hasClass("is-actived"));
-      // });
-    });
-
-    cardActive ? $(this).addClass("active") : console.log(cardActive);
-  });
-
-  function handleImageChange() {
-    $(stopBtn).prop("disabled", true);
-    $(spanTimer).text("");
-
-    clearInterval(intervalHandle);
-    clearTimeout(timeoutHandle);
-
-    if (usedImageIndexes.includes(currentImageIndex)) {
-      alert("image est déjà dans la sélection !");
-    } else {
-      usedImageIndexes.push(currentImageIndex);
-      console.log("USED", usedImageIndexes);
-      $("#image" + ++lastFilledImageNumber).attr(
-        "src",
-        pokemons[currentImageIndex].image
-      );
-    }
-
-    if (lastFilledImageNumber === 6) {
-      $(startBtn).text("C'est fini !");
-      $(startBtn).prop("disabled", true);
-    }
-  }
-
-  async function getPokemons(url) {
-    let body = await fetch(url);
-    let pokemons = await body.json();
-    return pokemons;
-  }
-
-  function getRandomNumber(max) {
-    return Math.floor(Math.random() * max);
-  }
+	//Get random number
+	function getRandomNumber(max) {
+		return Math.floor(Math.random() * max);
+	}
 });
--- assets/javascript/pokemonCards.js
+++ assets/javascript/pokemonCards.js
@@ -1,29 +1,29 @@
 function pokemonCards(pokemons, varAbilitie, container) {
-  let { name, level, image, background_color, abilities } = pokemons;
+	let { name, level, image, background_color, abilities } = pokemons;
 
-  let abilitie = abilities.map((abilitie) => {
-    const { name, icon, power, description } = abilitie;
+	let abilitie = abilities.map((abilitie) => {
+		const { name, icon, power, description } = abilitie;
 
-    return (varAbilitie += `
+		return (varAbilitie += `
   <div class="content-abilities">
      <div class="abilitie d-flex space-between">
       <div class="icon-name d-flex gap-40">
         <div class="icon">${icon}</div>
         <h6 class="name">${name}</h6>
       </div>
-      <b class="power">${power}</b>
+      <b class="numbers">${power}</b>
      </div>
      <p class="description">${description.slice(0, 90)}</p>
     </div>
     `);
-  });
+	});
 
-  let showCard = "";
+	let showCard = "";
 
-  showCard += `<div class="card id="card" style="background-color: ${background_color};">
+	showCard += `<div class="card id="card" style="background-color: ${background_color};">
   <div class="name-level d-flex space-between">
     <h5 id="name" class="name">${name}</h5>
-    <span id="level" class="level"><span class="tiny">Niv</span><b>${level} ${abilities[0].icon}</b></span>
+    <span id="level" class="level"><span class="tiny">Niv</span><b class="numbers">${level} <span class="icon">${abilities[0].icon}<span></b></span>
   </div>
   <div class="img" id="img">
     <img src=${image} id="main-image" class="main-img" />
@@ -31,7 +31,7 @@ function pokemonCards(pokemons, varAbilitie, container) {
   <div class="abilities" id="abilities">${abilitie}</div>
 </div>`;
 
-  container.html(showCard);
+	container.html(showCard);
 }
 
 export default pokemonCards;
